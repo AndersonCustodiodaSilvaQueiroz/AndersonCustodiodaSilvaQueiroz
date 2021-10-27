19812d61160ff7c06fcde3913219c127e278427e
Anderson Queiroz <plank7@hotmail.com>
2021-10-27 11:46:06 -0300

    Primeira parte treinamento TypeScript

---
+++ app/app.ts
@@ -0,0 +1,9 @@
+import { NegociacaoController } from "./controllers/negociacao-controllers.js";
+
+const controller = new NegociacaoController;
+const from  = document.querySelector('from');
+from.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+})
+
